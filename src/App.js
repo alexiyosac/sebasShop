447d99a775f2835d526bb1367342943fa09bebf4
alexiyosac <alexiyosa@gmail.com>
2024-09-20 16:33:31 -0500

    implementando el api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">
+  const url = "https://fakestoreapi.com/products/";
 
-  </div>;
+  const [todos, setTodos] = useState([]);
+
+  const fetchApi = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setTodos(data);
+    console.log(data);
+    <div>{data.title}</div>;
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  return (
+    <div>
+      {todos.map((codigo) => {
+        return (
+          <div>
+            <p>{codigo.id}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
