57881fa694ca1effbf73c7b8dd50bce26d7d931e
alexiyosac <alexiyosa@gmail.com>
2024-09-20 11:29:39 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-  hola
-    </div>
-  );
+  return <div className="App">
+
+  </div>;
 }
 
 export default App;
